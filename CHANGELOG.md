a00ad06df9f57c808cb067f3649178fe44cd84d6
hhh <1340545944@qq.com>
2018-08-06 20:40:34 +0800

    0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+### 0.1.1 - 2018-08-06
+
+- Update type declarations
+
 ## 0.1.0 - 2018-08-06
 
 - Initial release
--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,20 @@
-import { Component, ComponentConstructor, AnyProps } from "hele";
+import HEle = require("hele");
 
-declare namespace HEle {
-    export interface CreateClassOptions<P extends AnyProps = AnyProps, S = any, SS = any> {
-        constructor?: (this: Component<P, S, SS>, props: P) => void;
-        render(this: Component<P, S, SS>): any;
-        onWillMount?: (this: Component<P, S, SS>) => void;
-        onDidMount?: (this: Component<P, S, SS>) => void;
-        shouldUpdate?: (this: Component<P, S, SS>, oldStates: S, newStates: S) => boolean;
-        onWillUpdate?: (this: Component<P, S, SS>) => SS;
-        onDidUpdate?: (this: Component<P, S, SS>, snapshot: SS) => void;
-        onWillUnmount?: (this: Component<P, S, SS>) => void;
-        onDidUnmount?: (this: Component<P, S, SS>) => void;
-        onUncaughtError?: (this: Component<P, S, SS>, error: Error) => void;
-        defaultProps?: AnyProps;
-    }
-    export function createClass<P extends AnyProps = AnyProps, S = any, SS = any>(options: CreateClassOptions<P, S, SS>): ComponentConstructor<P, S, SS>;
-}
-
-export as namespace HEle;
 export = HEle;
+
+declare module "HEle" {
+    interface CreateClassOptions<P extends HEle.AnyProps = HEle.AnyProps, S = any, SS = any> {
+        constructor?: (this: HEle.Component<P, S, SS>, props: P) => void;
+        render(this: HEle.Component<P, S, SS>): any;
+        onWillMount?: (this: HEle.Component<P, S, SS>) => void;
+        onDidMount?: (this: HEle.Component<P, S, SS>) => void;
+        shouldUpdate?: (this: HEle.Component<P, S, SS>, oldStates: S, newStates: S) => boolean;
+        onWillUpdate?: (this: HEle.Component<P, S, SS>) => SS;
+        onDidUpdate?: (this: HEle.Component<P, S, SS>, snapshot: SS) => void;
+        onWillUnmount?: (this: HEle.Component<P, S, SS>) => void;
+        onDidUnmount?: (this: HEle.Component<P, S, SS>) => void;
+        onUncaughtError?: (this: HEle.Component<P, S, SS>, error: Error) => void;
+        defaultProps?: HEle.AnyProps;
+    }
+    function createClass<P extends HEle.AnyProps = HEle.AnyProps, S = any, SS = any>(options: CreateClassOptions<P, S, SS>): HEle.ComponentConstructor<P, S, SS>;
+}
